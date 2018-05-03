ec52783fe784c7d73dd82e4765195a104c6f0094
Тов.Жюков <tov-jukof@ya.ru>
2018-05-03 16:11:37 +0500

    clean

--- app/js/common.js
+++ app/js/common.js
@@ -1,41 +1,11 @@
 $(function() {
 
-	$(function () {
-
-		$("#menu").mmenu({
-			extensions: ["position-bottom", "fullscreen", "theme-black", "listview-50", "fx-panels-slide-up", "fx-listitems-drop", "border-offset"],
-			navbar: {
-				title: ""
-			},
-			navbars: [{
-				height: 2,
-				content: [
-					'<a href="#" class="fa fa-phone"></a>',
-					'<div class="logo"><img src="img/ms-logo.svg" /></div>',
-					'<a href="#" class="fa fa-envelope"></a>'
-				]
-			}, {
-				content: ["prev", "title"]
-			}]
-		}, {});
-		$(".mh-head.mm-sticky").mhead({
-			scroll: {
-				hide: 200
-			}
-		});
-		$(".mh-head:not(.mm-sticky)").mhead({
-			scroll: false
-		});
 
 
 
-		$('body').on('click',
-			'a[href^="#/"]',
-			function () {
-				alert("Thank you for clicking, but that's a demo link.");
-				return false;
-			}
-		);
-	});
+
+// $(".preloader").fadeOut();
+$(".preloader").console.log('dfdfdf');
+
 
 });
