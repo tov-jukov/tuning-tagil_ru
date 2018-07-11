c5fec64771186e807010961e35c09de56f793f12
tov-jukov <tov-jukof@ya.ru>
2018-07-11 09:05:18 +0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -39,9 +39,9 @@ gulp.task('js', function() {
 		'app/libs/jquery/dist/jquery.min.js',
 		// 'app/libs/MasonJS/dist/mason.min.js',
 		// 'app/libs/mosaic/js/mosaic.js',
-		'app/libs/justifiedGallery/dist/js/jquery.justifiedGallery.min.js',
 		'app/libs/photoswipe/dist/photoswipe.js',
 		'app/libs/photoswipe/dist/photoswipe-ui-default.js',
+		'app/libs/justifiedGallery/dist/js/jquery.justifiedGallery.min.js',
 		'app/libs/smartlid/smartlid.js',
 		'app/js/common.js', // Always at the end
 		])
