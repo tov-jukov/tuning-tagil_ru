5263144f69a2bfbe6f4b7deaec5023cfb86dae7c
tov-jukov <tov-jukof@ya.ru>
2018-07-12 15:37:01 +0500

    Update common.js, scripts.min.js, and 5 more files...

--- app/js/common.js
+++ app/js/common.js
@@ -170,3 +170,56 @@ function photoSwipe() {
 //     });
 // });
 
+
+//     /* 
+//      * just for this demo:
+//      */
+//     $('#showcode').toggle(
+//         function() {
+//             $(this).addClass('up').removeClass('down').next().slideDown();
+//         },
+//         function() {
+//             $(this).addClass('down').removeClass('up').next().slideUp();
+//         }
+//     );
+//     $('#panel').toggle(
+//         function() {
+//             $(this).addClass('show').removeClass('hide');
+//             $('#overlay').stop().animate( { left : - $('#overlay').width() + 20 + 'px' }, 300 );
+//         },
+//         function() {
+//             $(this).addClass('hide').removeClass('show');
+//             $('#overlay').stop().animate( { left : '0px' }, 300 );
+//         }
+//     );
+ $(function() {   
+    var $container 	= $('#am-container'),
+        $imgs		= $container.find('img').hide(),
+        totalImgs	= $imgs.length,
+        cnt			= 0;
+    
+    $imgs.each(function(i) {
+        var $img	= $(this);
+        $('<img/>').load(function() {
+            ++cnt;
+            if( cnt === totalImgs ) {
+                $imgs.show();
+                $container.montage({
+                    fillLastRow	: true,
+                    alternateHeight	: true,
+                    alternateHeightRange : {
+                        min	: 90,
+                        max	: 240
+                    },
+                    margin : 5
+                });
+                
+                /* 
+                 * just for this demo:
+                 */
+                // $('#overlay').fadeIn(500);
+            }
+        }).attr('src',$img.attr('src'));
+    });	
+    
+});
