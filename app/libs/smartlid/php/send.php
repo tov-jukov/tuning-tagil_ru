a33912c836bcb518579ad0bbd64819d14bccce81
Тов.Жюков <tov-jukof@ya.ru>
2018-08-06 02:23:30 +0500

    zz

---
+++ app/libs/smartlid/php/send.php
@@ -0,0 +1,12 @@
+<?php
+// Файлы phpmailer
+// require './phpmailer/class.phpmailer.php';
+// require './phpmailer/class.smtp.php';
+
+// require_once($_SERVER['DOCUMENT_ROOT'] . '/libs/smartlid/php/phpmailer/class.phpmailer.php');
+// require_once($_SERVER['DOCUMENT_ROOT'] . '/libs/smartlid/php/phpmailer/class.smtp.php');
+
+echo ($_SERVER['DOCUMENT_ROOT']);
+
+
+?>
