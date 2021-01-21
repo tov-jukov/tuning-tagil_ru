c7be5d1cddb816d1d537f61216c70ef0ed67a84b
tov-jukov[h] <tov-jukof@ya.ru>
2021-01-21 13:16:38 +0500

    Update kontakty.html and sender.php

--- app/libs/smartlid/sender.php
+++ app/libs/smartlid/sender.php
@@ -12,37 +12,35 @@ $mailSMTP = new SendMailSmtpClass('m-style-nt@yandex.ru', 'vcnbkmgfhjkm', 'ssl:/
 // $mailSMTP = new SendMailSmtpClass('логин', 'пароль', 'хост', 'порт', 'кодировка письма');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $messageText = '<p>Заявка на обратный звонок:</p><br>
+  $messageText = '<p>Заявка на обратный звонок:</p><br>
     <table>
       <tr>
         <td><b>Название страницы: </td>
-        <td>'.(isset($_POST['title'])?strip_tags($_POST['title']):'').'</td>
+        <td>' . (isset($_POST['title']) ? strip_tags($_POST['title']) : '') . '</td>
       </tr>
       <tr>
         <td><b>Адрес страницы: </b></td>
-        <td>'.(isset($_POST['href'])?strip_tags($_POST['href']):'').'</td>
+        <td>' . (isset($_POST['href']) ? strip_tags($_POST['href']) : '') . '</td>
       </tr>
       <tr>
         <td><b>Имя: </b></td>
-        <td>'.strip_tags($_POST['name']).'</td>
+        <td>' . strip_tags($_POST['name']) . '</td>
       </tr>
       <tr>
         <td><b>Телефон: </b></td>
-        <td>'.strip_tags($_POST['tel']).'</td>
+        <td>' . strip_tags($_POST['tel']) . '</td>
       </tr>
       <tr>
         <td><b>Почта: </b></td>
-        <td>'.(isset($_POST['email']) ?strip_tags($_POST['email']):'').'</td>
+        <td>' . (isset($_POST['email']) ? strip_tags($_POST['email']) : '') . '</td>
       </tr>
       <tr>
         <td><b>Сообщение: </b></td>
-        <td>'.strip_tags($_POST['text']).'</td>
+        <td>' . strip_tags($_POST['text']) . '</td>
       </tr>
     </table>';
-}
-else
-{
-    $messageText = 'Что то пошло не то';
+} else {
+  $messageText = 'Что то пошло не то';
 }
 
 $agreement = "<b>Дополнительно:</b> Отправитель дал согласие на обработку персональных данных</b>";
@@ -52,8 +50,8 @@ $messageSubject = 'Заявка с сайта';
 
 // от кого
 $from = array(
-    "Сайт", // Имя отправителя
-    "m-style-nt@yandex.ru" // почта отправителя
+  "Сайт", // Имя отправителя
+  "m-style-nt@yandex.ru" // почта отправителя
 );
 // кому отправка. Можно указывать несколько получателей через запятую
 $to = 'm-style-nt@yandex.ru';
@@ -62,12 +60,11 @@ $to = 'm-style-nt@yandex.ru';
 //$mailSMTP->addFile("test.jpg");
 
 // отправляем письмо
-$result =  $mailSMTP->send($to, $messageSubject, $messageText , $from);
+$result =  $mailSMTP->send($to, $messageSubject, $messageText, $from);
 // $result =  $mailSMTP->send('Кому письмо', 'Тема письма', 'Текст письма', 'Отправитель письма');
 
-if($result === true){
-    echo 'successmsgs';
-}
-else{
-    echo 'failmsgs';
+if ($result === true) {
+  echo 'successmsgs';
+} else {
+  echo 'failmsgs';
 }
